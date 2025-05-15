f553bc06b17434320280b7bcf8ce5ce3480a7091
lqian006 <lqian006@gmail.com>
2025-05-15 20:31:46 +0200

    Comienzo de la versión 3

--- Proyecto/navPoint.py
+++ Proyecto/navPoint.py
@@ -1,15 +1,31 @@
-with open ("Cat_nav.txt", "r", encoding="utf") as f:
-    lines=f.readlines()
-for i in range(len(lines)):
-    datos=lines[i].strip().split()
-
-    number=
-
-
-
 class NavPoint:
-    def __init__(self, number, name, latitude,longitude):
+    def __init__(self, number, name, latitude, longitude):
         self.number = number
         self.name = name
         self.latitude = latitude
         self.longitude = longitude
+
+
+lista_navpoints = []
+
+with open("Cat_nav.txt", "r", encoding="utf-8") as f:
+    for line in f:
+        datos = line.strip().split()
+        number = int(datos[0])
+        name = datos[1]
+        latitude = float(datos[2])
+        longitude = float(datos[3])
+        punto = NavPoint(number, name, latitude, longitude)
+        lista_navpoints.append(punto)
+
+
+x=int(input("Introduce un número de navpoint: "))
+
+for punto in lista_navpoints:
+    if punto.number == x:
+        print("Nombre:", punto.name)
+        print("Latitud:", punto.latitude)
+        print("Longitud:", punto.longitude)
+        break
+else:
+    print("Ese navpoint no existe.")
--- Proyecto/navSegment.py
+++ Proyecto/navSegment.py
@@ -1,5 +1,16 @@
+import math
 class NavSegment:
     def __init__(self, OriginNumber, DestinationNumber, Distance):
         self.OriginNumber = OriginNumber
         self.DestinationNumber = DestinationNumber
         self.Distance = Distance
+
+def Distance(OriginNumber, DestinationNumber):
+    return math.sqrt((DestinationNumber.x - OriginNumber.x) ** 2 + (DestinationNumber.y - OriginNumber.y) ** 2)
+
+class Node:
+    def __init__(self, name: str, x: float, y: float):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.neighbors = []
